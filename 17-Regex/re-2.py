70232caf987e382c15f68cbc2d71d9a2494a4999
attrigirish <41194400+attrigirish@users.noreply.github.com>
2019-07-26 16:54:09 +0530

    Add files via upload

--- 17-Regex/re-2.py
+++ 17-Regex/re-2.py
@@ -2,22 +2,41 @@ import re
 
 password=input("Enter Password : ")
 
-pass_pat=re.compile("^[0-9A-Za-z#_]{6,12}$")
+invalid_pat=re.compile("[!@$%^&*()]")
 digit_pat=re.compile("[0-9]")
 lower_pat=re.compile("[a-z]")
 upper_pat=re.compile("[A-Z]")
 
+isvalid=True
 
-if(pass_pat.search(password)!=None and digit_pat.search(password)!=None and lower_pat.search(password)!=None and upper_pat.search(password)!=None):
-    print("Password Valid")
+
+if(digit_pat.search(password)==None):
+    print("Minimum 1 Digit Required")
+    isvalid=False
+
+if(lower_pat.search(password)==None):
+    print("Minimum 1 Lower case Alphabet Reuired")
+    isvalid=False
+
+if(upper_pat.search(password)==None):
+    print("Minimum 1 Upper case Alphabet Required")
+    isvalid=False
+
+if(len(password)<6):
+    print("Minimum 6 characters required")
+    isvalid=False
+
+if(len(password)>12):
+    print("Maximum 12 characters required")
+    isvalid=False
+
+if(invalid_pat.search(password)!=None):
+    print("Special Characters are not allowed")
+    isvalid=False
+
+if(isvalid==True):
+    print("Password is valid")
 else:
-    if(digit_pat.search(password)==None):
-       print("Minimum 1 Digit Required")
-    if(lower_pat.search(password)==None):
-       print("Minimum 1 Lower case Alphabet Reuired")
-    if(upper_pat.search(password)==None):
-       print("Minimum 1 Upper case Alphabet Required")
-    if(len(password)<6):
-       print("Minimum 6 characters required")
-    if(len(password)>12):
-       print("Maximum 12 characters required")
+    print("Password is invalid")
+
+
